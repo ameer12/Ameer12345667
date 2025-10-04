4bbe09dc3b01b2c02812c79a212fd87850ccd2a3
ameer12 <fatenhs343@gmail.com>
2025-10-04 12:11:32 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import React from "react";
+import ContractInfo from "./components/ContractInfo";
+import AttestationViewer from "./components/AttestationViewer";
+import ComplianceSignature from "./components/ComplianceSignature";
 
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Asset Manager UI</h2>
+      <ContractInfo />
+      <AttestationViewer />
+      <ComplianceSignature />
+    </div>
+  );
+}
+
+export default App;
